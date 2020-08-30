53a35429074e330bf56eb9b36e464cdedbc1aaaa
David Purdy (aka Java Code Junkie) <45924598+JavaCodeJunkie@users.noreply.github.com>
2020-08-30 11:01:21 -0300

    Add files via upload
    JavaFX Project Template

---
+++ JavaFX Project (Template)/src/com/javacodejunkie/MainView.java
@@ -0,0 +1,32 @@
+package com.javacodejunkie;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainView {
+
+	public Stage stage;
+	private BorderPane root;
+	private Scene scene;
+
+	public MainView(Stage stage) {
+		this.stage = stage;
+		buildUI();
+	}
+
+	private void buildUI() {
+		root = new BorderPane();
+		
+		
+
+		scene = new Scene(root, 800, 500);
+		scene.getStylesheets().add("/com/javacodejunkie/stylesheet.css");
+		stage.setTitle("Java Code Junkie - ???");
+		stage.setScene(scene);
+	}
+
+	public void show() {
+		this.stage.show();
+	}
+}
